438ea363c663390ac60dcde4878e0a8a39610ecc
rjm <robjmass@gmail.com>
2024-06-27 13:51:13 -0400

    classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import './App.css';
 import React, { useState, useRef, useEffect, ChangeEvent, KeyboardEvent } from 'react';
-
+import {Asset, Cash, AccountsReceivable } from './Assets';
 
 
 const App: React.FC = () => {
-    const [cash, setCash] = useState<number>(100.00);
-    const [debt, setDebt] = useState<number>(1000.00);
+    const cash = new Cash(100.00);
+    const receivable = new AccountsReceivable(500.00);
 
-    const USD = (value: number): string => {
-        return new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        }).format(value);
-    };
+
     
     return (
         <div>
-            <p>{"Cash: " + USD(cash)}</p>
-            <p>{"Debt: " + USD(debt)}</p>
+            <p>{cash.USD()}</p>
+            <p>{receivable.USD()}</p>
         </div>
     );
 }
