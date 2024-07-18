c1eb76e00f2adfbdd013d6536b73f44e0303d555
rjm <robjmass@gmail.com>
2024-07-18 16:24:54 -0400

    cleanup

--- src/store/Customer.tsx
+++ src/store/Customer.tsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import './Store.css';
-//
-// interface CustomerProps {
-//     position: { row: number; col: number };
-//     style?: React.CSSProperties;
-// }
-//
-// const Customer: React.FC<CustomerProps> = ({ position, style }) => {
-//     return (
-//         <div
-//             className="customer"
-//             style={{
-//                 ...style,
-//                 gridRow: position.row + 1,
-//                 gridColumn: position.col + 1,
-//
-//             }}
-//         />
-//     );
-// };
-//
-// export default Customer;
-//
-
 import React from 'react';
 import './Store.css';
 import { Product } from '../inventory/Product';
@@ -53,25 +28,21 @@ export default class Customer extends React.Component<CustomerProps> implements 
     public get attemptedProducts(): { product: Product, bought: boolean }[] { return this._attemptedProducts; }
 
     private generateSpree(): number {
-        const p = 0.75;
-        let count = 1;
-        while (Math.random() < p) {
-            count++;
-        }
+        let p: number = 0.75, count: number = 1;
+        while (Math.random() < p) count++;
         return count;
     }
 
     public tryBuy(product: Product): boolean {
-        const randomValue = Math.random();
-        const bought = randomValue <= product.getDemand();
+        const randomValue: number = Math.random();
+        const bought: boolean = randomValue <= product.getDemand();
         this._attemptedProducts.push({ product, bought });
         if (bought) {
             console.log(`${this._name} bought ${product.getName()} with demand ${product.getDemand()}.`);
             return true;
-        } else {
-            console.log(`${this._name} did not buy ${product.getName()} with demand ${product.getDemand()}.`);
-            return false;
         }
+        console.log(`${this._name} did not buy ${product.getName()} with demand ${product.getDemand()}.`);
+        return false;
     }
 
     public render() {
