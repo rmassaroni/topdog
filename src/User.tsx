0f52b8e9254bc31f0c233bc82f4402556a5ade5f
rjm <robjmass@gmail.com>
2024-07-22 12:51:03 -0400

    user

---
+++ src/User.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import { useAuth } from '../context/AuthContext';
+
+const User = () => {
+    // const { user } = useAuth();
+    // return <div>{user.name}</div>;
+}
+
+export default User;
