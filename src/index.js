536e9b0f89b0903e0727a0044cf64c67514452a8
rjm <robjmass@gmail.com>
2024-08-22 16:24:58 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
-import { GlobalProvider } from './GlobalContext';
+// import { GlobalProvider } from './GlobalContext';
 
 window.greet = (name) => {
     return `Hello, ${name}!`;
@@ -12,12 +12,14 @@ window.greet = (name) => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <GlobalProvider>
             <App />
-        </GlobalProvider>
     </React.StrictMode>
 );
-
+    // <React.StrictMode>
+    //     <GlobalProvider>
+    //         <App />
+    //     </GlobalProvider>
+    // </React.StrictMode>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
