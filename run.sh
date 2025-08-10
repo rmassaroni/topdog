ee80f95c1cf5e3de35716634e6ab3ba6c8418922
rmassaroni <robjmass@gmail.com>
2025-08-09 22:52:57 -0400

    src folder

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-g++ main.cpp Game.cpp TextureManager.cpp GameObject.cpp -o game $(pkg-config --cflags --libs sdl2 SDL2_image)
+cd src
+g++ main.cpp Game.cpp TextureManager.cpp GameObject.cpp -o ../game $(pkg-config --cflags --libs sdl2 SDL2_image)
+cd ..
 ./game
