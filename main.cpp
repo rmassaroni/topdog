20c5632cbb073c9a2a0a242e4fb4d24d7b4c86f8
rmassaroni <robjmass@gmail.com>
2025-08-09 22:06:32 -0400

    fps

--- main.cpp
+++ main.cpp
@@ -3,13 +3,28 @@
 Game *game = nullptr;
 
 int main(int argc, char* argv[]) {
+
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+    
+    Uint32 frameStart;
+    int frameTime;
+
     game = new Game();
     game->init("SDL Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
     while (game->running()) {
+        frameStart = SDL_GetTicks();
+        
         game->handleEvents();
         game->update();
         game->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if (frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
 
     game->clean();
